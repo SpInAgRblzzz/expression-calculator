6a5a2be714141115d9a41d719e536d358fec648e
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-10-08 22:02:54 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) { 
+function expressionCalculator(expr) {
+    debugger
     let task = expr;    
 
     function isOperator(candidate){
@@ -11,11 +12,13 @@ function expressionCalculator(expr) {
     };
 
     function solvePart(part,operators){
-
+        debugger
         for(let i = 0; i<part.length; i++){
             if(part[i] === operators[0]||part[i] === operators[1]){
                 const operator = part[i];
+
                 function calculate(a,b){
+                    debugger
                     a=a.replace('NEG','-');
                     b=b.replace('NEG','-');
                     if(operator === '*'){return a*b}
@@ -46,15 +49,42 @@ function expressionCalculator(expr) {
                     }
                     subPartEnd++;
                 }
-                const subPart = part.slice(subPartStart+1,subPartEnd);                
-                const subPartResult = subPart.split(operator).reduce(calculate);
+                const subPart = part.slice(subPartStart+1,subPartEnd);
+                debugger
+                const subPartResult = subPart.split(operator).reduce(calculate).toFixed(20);
                 part = part.replace(subPart, String(subPartResult).replace('-','NEG'));
+                debugger
                 i = subPartStart+1;
             }
         }
         return part;
     };
     
+    //brackets
+    for(let openBr = task.length - 1; openBr>=0; openBr--){
+        if(task[openBr]==='('){
+            for(let closeBr = openBr, len= task.length; closeBr<len; closeBr++){
+                if(task[closeBr]===')'){
+                    const brExpression = task.slice(openBr, closeBr+1)
+                    const brInside = task.slice(openBr+1,closeBr);
+                    debugger
+                    const brInsideResult = solvePart( solvePart(brInside,['*','/']), ['+','-']);
+                    task = task.replace(brExpression, brInsideResult);
+                    debugger
+                    break
+                } else {
+                    if(closeBr === len-1){
+                        throw new Error('ExpressionError: Brackets must be paired');
+                    }
+                }
+            }
+        }
+    }
+
+    if(task.indexOf(')') !== -1){
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+
     const result = +solvePart( solvePart(task,['*','/']), ['+','-']).replace('NEG','-');
 
     return +result;

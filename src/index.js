a870846363a5d1bd98c4b4c87e5ec8acae30936b
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-10-08 20:53:59 +0300

    bracketless calculations added

--- src/index.js
+++ src/index.js
@@ -3,117 +3,63 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {    
-    let task = expr;
+function expressionCalculator(expr) { 
+    let task = expr;    
 
-    function solvePart(operator, ind){
-    }
+    function isOperator(candidate){
+        return candidate==='+'||candidate==='-'||candidate==='*'||candidate==='/';
+    };
 
-    //1st priority
-    for(let i = 0; i < task.length; i++){
-        if(task[i] === '*'){
-            solvePart('*',i);
-            i = 0;
+    function solvePart(part,operators){
+
+        for(let i = 0; i<part.length; i++){
+            if(part[i] === operators[0]||part[i] === operators[1]){
+                const operator = part[i];
+                function calculate(a,b){
+                    a=a.replace('NEG','-');
+                    b=b.replace('NEG','-');
+                    if(operator === '*'){return a*b}
+                    if(operator === '/'){
+                        if(+b===0){
+                            throw new Error('TypeError: Devision by zero.')
+                        }
+                        return a/b
+                    }
+                    if(operator === '+'){return +a + +b}
+                    if(operator === '-'){return a-b}
+                }
+
+                let subPartStart = i - 1;
+                while(!isOperator( part[subPartStart] )){
+                    if(subPartStart===0){
+                        subPartStart=-1;
+                        break
+                    };
+                    subPartStart--;
+                }
+
+                let subPartEnd = i + 1;
+                while(!isOperator( part[subPartEnd] )){
+                    if(subPartEnd === part.length-1){
+                        subPartEnd++;
+                        break
+                    }
+                    subPartEnd++;
+                }
+                const subPart = part.slice(subPartStart+1,subPartEnd);                
+                const subPartResult = subPart.split(operator).reduce(calculate);
+                part = part.replace(subPart, String(subPartResult).replace('-','NEG'));
+                i = subPartStart+1;
+            }
         }
+        return part;
+    };
+    
+    const result = +solvePart( solvePart(task,['*','/']), ['+','-']).replace('NEG','-');
 
-        if(task[i] === '/'){
-            solvePart('/',i);
-            i = 0;
-        }
-    }
-
-    //2nd priority
-    for(let i = 0; i < task.length; i++){
-        if(task[i] === '+'){
-            solvePart('+',i);
-            i = 0;
-        }
-
-        if(task[i] === '-'){
-            solvePart('-',i);
-            i = 0;
-        }
-    }
-
-    return task;
+    return +result;
 }
 
 module.exports = {
     expressionCalculator
 }
-
-/*for(let bracketOpen = expr.length -1; bracketOpen >= 0; bracketOpen--){
-        if(task[bracketOpen] === '('){
-            const bracketClose = task.indexOf(')', bracketOpen);
-            const bracketInside = task.slice(bracketOpen + 1, bracketClose);
-            task = task.replace(task.slice(bracketOpen,bracketClose + 1), String(expressionCalculator(bracketInside)));
-        }
-    }
-
-    /////bracket error test
-    
-    function operationPositionCondition(ind){
-        debugger
-        return task[ind] === '*'|| task[ind] === '/'|| task[ind] === '+'|| task[ind] === '-'
-    }
-    
-    function solveOperation(operator, ind){
-        debugger
-        function getResult(a,b){
-            debugger
-            if(operator === '*'){return a * b}
-            if(operator === '/'){return a / b}
-            if(operator === '+'){return +a + +b}
-            if(operator === '-'){return a - b}
-        }
-
-        let operationStart = ind-1;
-            while(!operationPositionCondition(operationStart)){
-                operationStart--;
-                if(operationStart === 0){
-                    operationStart--;
-                    break;
-                };
-            }
-
-        let operationEnd = ind+1;
-        while(!operationPositionCondition(operationEnd)){
-            operationEnd++;
-            if(operationEnd === task.length){
-                operationEnd++
-                break
-            };
-        }
-            
-        const operation = task.slice(operationStart+1,operationEnd-1);
-        const operationResult = operation.split(operator).reduce(getResult);
-        task = task.replace(operation, operationResult);
-    }
-    
-    //1st priority
-    
-    for(let i = 0, len = task.length; i < len; i++){
-        if(task[i] === '*'){
-            solveOperation('*',i);
-            i = 0;
-        }
-        if(task[i] === '/'){
-            solveOperation('/',i);
-            i = 0;
-        }
-    }
-
-
-    //2nd priority
-    for(let i = 0, len = task.length; i < len; i++){
-        if(task[i] === '+'){
-            solveOperation('+',i);
-            i = 0;
-        }
-        if(task[i] === '-'){
-            solveOperation('-',i);
-            i = 0;
-        }
-    }
-
-    return +task*/
